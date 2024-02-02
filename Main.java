670b92fbfbaa5bacd2dbcc60288992ba76db19f8
Abisagy Nafula <abisakyn@gmail.com>
2024-02-02 13:00:34 +0300

    third

--- Main.java
+++ Main.java
@@ -2,12 +2,13 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner read= new Scanner(System.in);
+        System.out.println("Enter rental Income:");
         int rentalIncome=read.nextInt();
+        System.out.println("Enter property value");
         int propertyValue=read.nextInt();
         double annualIncome =(rentalIncome*12);
         double rentalYield = ((annualIncome/propertyValue)*100);
-        System.out.println(annualIncome);
-        System.out.println(rentalYield +"%");
+        System.out.println("Rental Yield="+ rentalYield +"%");
     }
         }
 
